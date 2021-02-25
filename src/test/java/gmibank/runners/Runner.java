445c9def1c8937e5605f4a418a54edb48c0a96bf
gamzedeniz07 <gmzzdnzz07@gmail.com>
2021-02-25 15:49:52 +0300

    gamze

--- src/test/java/gmibank/runners/Runner.java
+++ src/test/java/gmibank/runners/Runner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features="src/test/resources/features",
         glue="gmibank/stepdefinitions",
         dryRun = false,
-        tags="@wait"
+        tags="@wip"
 )
 
 public class Runner {
