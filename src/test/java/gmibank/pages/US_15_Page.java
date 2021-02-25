6fe6f6be66c5297d345476a9027214ab0f3ef120
begumyilmaz <begumyilmaz3495@gmail.com>
2021-02-26 01:07:38 +0300

    US_15_16

--- src/test/java/gmibank/pages/US_15_Page.java
+++ src/test/java/gmibank/pages/US_15_Page.java
@@ -1,16 +1,14 @@
 package gmibank.pages;
 
 import gmibank.utilities.Driver;
-import org.apache.xmlbeans.SchemaTypeLoader;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
-public class US_15_16_Page {
-    public US_15_16_Page() {
+public class US_15_Page {
+    public US_15_Page() {
         PageFactory.initElements(Driver.getDriver(), this); }
 
     @FindBy(xpath = "//li[@id='entity-menu']")
@@ -30,6 +28,9 @@ public class US_15_16_Page {
 
     @FindBy (xpath = "//tbody//tr//td[3]")
     public List<WebElement> accountBalanceListesi;
+
+    @FindBy (xpath = "(//table[@class='table table-striped'])[2]")
+    public WebElement transactionTable;
 
 }
 
--- src/test/java/gmibank/stepdefinitions/US_16_Stepdefinitions.java
+++ src/test/java/gmibank/stepdefinitions/US_16_Stepdefinitions.java
@@ -1,4 +1,57 @@
 package gmibank.stepdefinitions;
 
+import gmibank.pages.US_16_Page;
+import gmibank.utilities.ConfigReader;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.support.ui.Select;
+
 public class US_16_Stepdefinitions {
+
+    US_16_Page us_16_page = new US_16_Page();
+
+    @And("kullanici My Operations'a tiklar")
+    public void kullaniciMyOperationsATiklar() {
+        us_16_page.myOperation_button.click();
+    }
+
+    @And("kullanici My Accounts'a tiklar")
+    public void kullaniciMyAccountsATiklar() {
+        us_16_page.myAccounts_button.click();
+    }
+
+
+    @And("From DropDown'indan bir hesap secer")
+    public void fromDropDownIndanBirHesapSecer() {
+        Select options = new Select(us_16_page.fromAccountDropDown);
+        options.selectByValue(ConfigReader.getProperty("investing_account_id"));
+    }
+
+    @And("To DropDown'indan bir hesap secer")
+    public void toDropDownIndanBirHesapSecer() {
+        Select options = new Select(us_16_page.toAccountDropDown);
+        options.selectByValue(ConfigReader.getProperty("saving_account_id"));
+    }
+
+    @And("Balance kismina gecerli bir para miktarini yazar")
+    public void balanceKisminaGecerliBirParaMiktariniYazar() {
+        us_16_page.balanceTextBox.sendKeys(ConfigReader.getProperty("gecerli_balance_miktari"));
+    }
+
+    @And("Description kismina aciklama yazar")
+    public void descriptionKisminaAciklamaYazar() {
+        us_16_page.descriptionTextBox.sendKeys(ConfigReader.getProperty("description_message"));
+    }
+
+    @And("Make Transfer butonuna tiklar")
+    public void makeTransferButonunaTiklar() {
+        us_16_page.makeTransferButton.click();
+    }
+
+
+    @Then("kullanici para transferi yapildigini dogrular")
+    public void kullaniciParaTransferiYapildiginiDogrular() {
+        Assert.assertEquals(us_16_page.succesfullMessage, "Transfer is succesfulL");
+    }
 }
